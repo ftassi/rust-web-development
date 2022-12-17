f8edfe9ad187006c3019c5ea3ec76509bebec420
Francesco Tassi <tassi.francesco@gmail.com>
2022-12-17 17:50:23 +0100

    Add some basic cors example

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use serde::Serialize;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
-use warp::{http::StatusCode, reject::Reject, Filter, Rejection, Reply};
+use warp::{http::Method, http::StatusCode, reject::Reject, Filter, Rejection, Reply};
 
 #[derive(Serialize, Debug)]
 struct Question {
@@ -54,7 +54,7 @@ async fn get_questions() -> Result<impl Reply, Rejection> {
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-    if let Some(_) = r.find::<InvalidId>() {
+    if let Some(InvalidId) = r.find() {
         Ok(warp::reply::with_status(
             "Invalid ID",
             StatusCode::UNPROCESSABLE_ENTITY,
@@ -69,12 +69,18 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
 
 #[tokio::main]
 async fn main() {
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_methods(&[Method::GET, Method::POST, Method::PUT, Method::DELETE])
+        .allow_header("content-type");
+
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
         .and_then(get_questions)
         .recover(return_error);
 
-    let routes = get_items;
+    let routes = get_items.with(cors);
+
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
